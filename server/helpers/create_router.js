369694c2b7a82e8c28c5eb43bd481be863b42c3b
Joseph Young <jsyoung0891@gmail.com>
2019-05-24 14:53:22 +0100

    finished and tested CRD functionality

--- server/helpers/create_router.js
+++ server/helpers/create_router.js
@@ -34,6 +34,24 @@ const createRouter = function(collection) {
         .insertOne(newItem)
         .then(() => collection.find().toArray())
         .then((docs) => res.json(docs))
+        .catch((err) => {
+            console.error(err)
+            res.status(500)
+            res.json({status: 500, error: err})
+        })
+    })
+
+    router.delete('/:id', (req, res) => {
+        const id = req.params.id
+        collection
+        .deleteOne({_id: ObjectID(id)})
+        .then(() => collection.find().toArray())
+        .then((docs) => res.json(docs))
+        .catch((err) => {
+            console.error(err)
+            res.status(500)
+            res.json({status: 500, error: err})
+        })
     })
 
     return router
