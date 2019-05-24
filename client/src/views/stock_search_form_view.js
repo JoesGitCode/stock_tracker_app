1fae633c021d43d124120ffb1724ec5734596559
James Budge <thebudgie@gmail.com>
2019-05-24 11:46:02 +0100

    adds search box and publishes the data

---
+++ client/src/views/stock_search_form_view.js
@@ -0,0 +1,17 @@
+const PubSub = require('../helpers/pub_sub.js')
+
+const StockSearchView = function (element) {
+  this.element = element
+}
+
+StockSearchView.prototype.bindEvents = function () {
+  this.element.addEventListener('submit', (event) =>{
+    console.log("HELLLLLLOOOOOO");
+    event.preventDefault()
+    const data = event.target.stockname.value
+    PubSub.publish('SearchFormView:ticker-selected', data)
+  })
+
+};
+
+module.exports = StockSearchView
