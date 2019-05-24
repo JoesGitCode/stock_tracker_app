8c77e91285605dcf8d6b590bd9343774b426bdf8
Mateusz <mateusz.trajdos@gmail.com>
2019-05-24 12:21:36 +0100

    Finished server JS file

--- server/server.js
+++ server/server.js
@@ -1,2 +1,28 @@
 const express = require('express');
 const app = express();
+const path = require('path');
+const parser = require('body-parser');
+const MongoClient = require('mongodb').MongoClient;
+const createRouter = require('./helpers/create_router.js');
+
+
+const publicPath = path.join(__dirname, '../client/public');
+app.use(express.static(publicPath));
+
+
+app.use(parser.json());
+
+
+
+MongoClient.connect('mongodb://localhost:27017')
+.then((client) =>{
+  const db = client.db('stock_portfolio');
+  const stocksCollection = db.collection('stocks');
+  const stocksRouter = createRouter(stocksCollection);
+  app.use('/api/stocks', stocksRouter);
+
+})
+
+app.listen(3000, function(){
+  console.log(`Listening on Port ${this.address().port}`);
+});
