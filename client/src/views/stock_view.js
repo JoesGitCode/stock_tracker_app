9b54f7bc58650ff5cc263e7f93e0f3a76223d4b9
Stephen Bradley <stephen.a.bradley90@outlook.com>
2019-05-26 13:32:41 +0100

    secondary info being rendered to the screen

--- client/src/views/stock_view.js
+++ client/src/views/stock_view.js
@@ -10,15 +10,42 @@ StockView.prototype.render = function (companyInfo) {
   companyContainer.id = 'companySearch'
   this.container.appendChild(companyContainer)
 
+  console.log(companyInfo);
+
   const companySymbol = this.createSymbol(companyInfo.symbol)
   companyContainer.appendChild(companySymbol)
+
+  const companyRevenue = this.createRevenue(companyInfo.financials[0]["Revenue"])
+  companyContainer.appendChild(companyRevenue)
+
+  const companyGrossProfit = this.createGrossProfit(companyInfo.financials[0]["Revenue Growth"])
+  companyContainer.appendChild(companyGrossProfit)
+
+  // const companyRevenue = this.createRevenue(companyInfo.financials[0].Revenue)
+  // companyContainer.appendChild(companyRevenue)
+  //
+  // const companyRevenue = this.createRevenue(companyInfo.financials[0].Revenue)
+  // companyContainer.appendChild(companyRevenue)
 };
 
 StockView.prototype.createSymbol = function(textContent) {
-  const symbol = document.createElement('h3')
+  const symbol = document.createElement('h1')
   symbol.textContent = textContent
   return symbol
 };
+
+StockView.prototype.createRevenue = function (textContent) {
+  const revenue = document.createElement('h4')
+  revenue.textContent = textContent
+  return revenue
+};
+
+StockView.prototype.createGrossProfit = function (textContent) {
+  const grossProfit = document.createElement('h4')
+  grossProfit.textContent = textContent
+  return grossProfit
+};
+
 
 
 module.exports = StockView;
