e7877122c884e2eeaabfc85464425ef917c7fc0e
Joseph Young <jsyoung0891@gmail.com>
2019-08-10 21:06:02 +0100

    added cors package to try and solve heroku cors error

--- server/server.js
+++ server/server.js
@@ -4,6 +4,9 @@ const path = require("path");
 const parser = require("body-parser");
 const MongoClient = require("mongodb").MongoClient;
 const createRouter = require("./helpers/create_router.js");
+const cors = require("cors");
+
+app.use(cors());
 
 const publicPath = path.join(__dirname, "../client/public");
 app.use(express.static(publicPath));
